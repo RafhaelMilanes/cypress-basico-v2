7444c5a18b8b1f9c8cb96e29b164ea86836c6a6a
Rafhael Milanes <rafhael.milanes@lekto.com.br>
2024-10-09 23:16:07 -0300

    ajustment baseUrl

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      baseUrl: 'http://localhost:3000'
     },
     viewportWidth: 1920, 
     viewportHeight: 1080, 
