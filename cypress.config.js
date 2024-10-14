fad5056178fcfb1c8e377060d64b8c9aeb49b545
Rafhael Milanes <rafhael.milanes@lekto.com.br>
2024-10-14 16:33:51 -0300

    ajuste arquivo de configuração cypress

--- cypress.config.js
+++ cypress.config.js
@@ -6,6 +6,7 @@ module.exports = defineConfig({
       // implement node event listeners here
       baseUrl: 'http://localhost:3000'
     },
+    video:true,
     viewportWidth: 1920, 
     viewportHeight: 1080, 
   },
